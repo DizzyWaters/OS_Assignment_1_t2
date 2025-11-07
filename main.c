95292ea92a76c9e24fee28077fafd536a810eee0
DizzyServer <mkulvanovskis@gmail.com>
2025-11-07 11:27:56 -0500

    fixed main func

--- main.c
+++ main.c
@@ -5,7 +5,8 @@ int main(int argc, char **argv)
 {
     double maxDist;
     int line = find_farthest_point("my_data.dat", &maxDist);
-	printf("This is max dist %d \n", line);
+	printf("Farthest point found at line: %d \n", line);
+    printf("Distance from origin: %.2f\n", maxDist);
 	return 0;
 }
 
